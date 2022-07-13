54c347c224aee02c580daef75133c5835984403e
bettertextAyoub05 <elouazzaniayoub05@gmail.com>
2022-07-13 17:41:19 +0200

    nice work

---
+++ testing math/age.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+    int age,dob;
+
+    // int a,b,m;
+    // printf("write any nembre you want :\n");
+    // scanf("%d",&a);
+    // printf("write the seconde nembre you want :\n");
+    // scanf("%d",&b);
+    // m=a+b;
+    // printf("\n %d + %d = %d \n" , a , b , m );
+
+    printf(" please right your date of birth : ");
+    scanf("%d",&dob);
+    age = 2022 - dob;
+    printf(" your age is :%d",age);
+    
+    
+    }
